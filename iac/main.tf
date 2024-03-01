37cc902a2bf806d337ac4042ae89ec9a0d137c09
yuyatinnefeld <yuyawamo@gmail.com>
2024-03-01 01:50:20 +0100

    feat: activate aks cluster module

--- iac/main.tf
+++ iac/main.tf
@@ -35,9 +35,9 @@ module "container_registry" {
   depends_on                        = [module.storage_account]
 }
 
-# module "aks_cluster" {
-#   source                         = "./modules/aks-cluster"
-#   env                            = var.env
-#   azure_container_resource_group = var.azure_container_resource_group
-#   depends_on                     = [module.container_registry]
-# }
+module "aks_cluster" {
+  source                         = "./modules/aks-cluster"
+  env                            = var.env
+  azure_container_resource_group = var.azure_container_resource_group
+  depends_on                     = [module.container_registry]
+}
