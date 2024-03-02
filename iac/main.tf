86b8e845a3465f194bcb62236a7fe3507c7964f7
yuyatinnefeld <yuyawamo@gmail.com>
2024-03-03 00:07:08 +0100

    test: update terraform for testing

--- iac/main.tf
+++ iac/main.tf
@@ -10,12 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-
   tenant_id       = var.ARM_TENANT_ID       # ANALOG GCP ORGANIZATION_ID
   subscription_id = var.ARM_SUBSCRIPTION_ID # ANALOG GCP PROJECT_ID
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
-
 }
 
 module "storage_account" {
