5eb1a99712ac3209e055d8ecb55541f2efc11395
yuyatinnefeld <yuyawamo@gmail.com>
2024-04-23 12:17:37 +0200

    refactor: update terraform setup

--- iac/0_tf_run.sh
+++ iac/0_tf_run.sh
@@ -1,6 +1,6 @@
-##################################### USE TERRAFORM VIA SERVICE PRINCIPAL #####################################
+# SETUP TERRAFORM ENV VARS
 export AZURE_TENANT_DEV="xxx"
-export SUBSCRIPTION_ID="xxx"
+export AZURE_SUBSCRIPTION_ID_DEV="xxx"
 export AZURE_SERVICE_PRINCIPAL_APP_ID_DEV="xxx"
 export AZURE_SERVICE_PRINCIPAL_PASSWORD_DEV="xxx"
 export AZURE_CLIENT_ID_DEV="xxx"
@@ -9,6 +9,7 @@ export AZURE_STATE_FILE_STORAGE="xxx"
 export AZURE_STORAGE_RESOURCE_GROUP="xxx"
 export ENV="xxx"
 
+# PASS-THOUGH INTO TERRAFORM CONFIG VARS 
 export TF_VAR_ARM_TENANT_ID=$AZURE_TENANT_DEV
 export TF_VAR_ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID_DEV
 export TF_VAR_ARM_CLIENT_ID=$AZURE_CLIENT_ID_DEV
@@ -16,4 +17,8 @@ export TF_VAR_ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
 
 # !!! THIS MUST BE RUN BEFORE CI/CD PIPELINE TF SCRIPTS RUNNING !!!
 export ARM_ACCESS_KEY=$(az storage account keys list --resource-group $AZURE_STORAGE_RESOURCE_GROUP-$ENV --account-name $AZURE_STATE_FILE_STORAGE --query '[0].value' -o tsv)
+echo $ARM_ACCESS_KEY
+
+# RUN TF SCRIPTS
 terraform init -backend-config="env/dev.tfbackend"
+terraform plan -var-file=env/dev.tfvars -out=tfplan
